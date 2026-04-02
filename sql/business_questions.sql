15b5bb9f5d08fd516a1da1e7da42d66db050d935
Alice Patel <alicepatel071103@gmail.com>
2026-04-02 12:03:55 -0600

    Add files via upload

--- sql/business_questions.sql
+++ sql/business_questions.sql
@@ -20,33 +20,33 @@ SUM(cast(replace(Subscription_Revenue_Millions,'$','') as decimal(4,1))) as 'Tot
  order by Views_Millions Desc
  limit 10;
 
--- Average rating by genre
+-- 4. Average rating by genre
 Select genre, 
 Round(avg(rating),1) as 'Rating'
 from netflix
 group by genre 
 order by Rating Desc;
 
--- 4. Best performing genre (by views)
+-- 5. Best performing genre (by views)
 Select genre , Round(AVG(Views_Millions),1) as 'view'
 from netflix
 group by genre 
 order by view Desc
 limit 1;
  
- -- 5. Highest Rated Title
+ -- 6. Highest Rated Title
  Select Title, Rating
  from netflix
  order by rating desc
  limit 1;
  
- -- 6. Countries with most content 
+ -- 7. Countries with most content 
  Select country, count(title) as 'Total Content'
  from netflix 
  group by country 
  Order by 'Total Content' desc;
 
--- 7. High views but low revenue 
+-- 8. High views but low revenue 
 Select title, Views_Millions, Subscription_Revenue_Millions
 from netflix
 order by Views_Millions Desc
